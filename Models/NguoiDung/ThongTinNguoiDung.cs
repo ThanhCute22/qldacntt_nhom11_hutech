3eba7941ea07bcf9cd5293a93ac2f299a37f9d96
ThanhCute22 <hoangthimythanh95@gmail.com>
2025-12-08 08:38:18 +0700

    Add files via upload

---
+++ Models/NguoiDung/ThongTinNguoiDung.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebsiteBanHang.Models.NguoiDung
+{
+    public class ThongTinNguoiDung : IdentityUser
+    {
+        [Required]
+        public string HoTen { get; set; }
+        public string? DiaChi { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public string? SDT { get; set; }
+        public bool IsSuperAdmin { get; set; } // Thêm thuộc tính này
+    }
+}
---
+++ Models/QuanLyLienHe/LienHeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteBanHang.Models.QuanLyLienHe
+{
+    public class LienHeViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập họ và tên")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Họ và tên phải từ 2 đến 100 ký tự")]
+        public string HoTen { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Vui lòng nhập email hợp lệ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [RegularExpression(@"^\d{10,11}$", ErrorMessage = "Số điện thoại phải là 10 hoặc 11 số")]
+        public string SoDienThoai { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập nội dung")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Nội dung phải từ 10 đến 1000 ký tự")]
+        public string NoiDung { get; set; }
+    }
+}
