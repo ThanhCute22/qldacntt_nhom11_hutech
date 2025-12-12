17a2aac80f6aa79faa1660b2eb6aac6593bba555
ThanhCute22 <hoangthimythanh95@gmail.com>
2025-12-12 09:48:38 +0700

    Add files via upload

---
+++ Areas/Admin/Controllers/ThongKeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using WebsiteBanHang.Repositories.I;
+using WebsiteBanHang.Repositories.I.QuanLyHoaDon;
+using WebsiteBanHang.Repositories.I.QuanLyThongKe;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+       [Area("Admin")]
+
+    public class ThongKeController : Controller
+    {
+
+        private readonly ApplicationDbContext _context; // Thêm để truy cập trực tiếp vào DbContext
+        private readonly IWebHostEnvironment _environment;
+        private readonly IThongKeRepository _thongkeRepository;
+
+        public ThongKeController(
+            ApplicationDbContext context,
+            IWebHostEnvironment environment,
+            IThongKeRepository thongKeRepository
+            )
+        {
+            _context = context;
+            _environment = environment;
+            _thongkeRepository = thongKeRepository;
+
+        }
+
+        public IActionResult Index()
+        {
+          
+
+            var count_SanPham = _context.SanPham.Count();
+            var count_DanhMuc = _context.DanhMuc.Count();
+            var count_HoaDon = _context.HoaDon.Count();
+            var count_User = _context.KhachHang.Count();
+            var count_Kho = _context.Kho.Count();
+            var count_PTTT = _context.PTTT.Count();
+            var count_VaiTro = _context.Roles.Count();
+            ViewBag.CountSanPham = count_SanPham;
+            ViewBag.CountDanhMuc = count_DanhMuc;
+            ViewBag.CountHoaDon = count_HoaDon;
+            ViewBag.CountUser = count_User;
+            ViewBag.CountKho = count_Kho;
+            ViewBag.ContPTTT = count_PTTT;
+            ViewBag.ContVaiTro = count_VaiTro;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetChartData(DateTime? startDate = null, DateTime? endDate = null)
+        {
+            var data = await _thongkeRepository.GetThongKeAsync(startDate, endDate);
+            return Json(data);
+        }
+    }
+}
---
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,45 @@
+﻿
+//logout.cshtml.cs
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebsiteBanHang.Models.NguoiDung;
+
+namespace WebsiteBanHang.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ThongTinNguoiDung> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+       
+
+        public LogoutModel(SignInManager<ThongTinNguoiDung> signInManager, 
+            ILogger<LogoutModel> logger
+            )
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+            
+        }
+
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            _logger.LogInformation("Bắt đầu quá trình đăng xuất cho người dùng: {UserName}", User?.Identity?.Name);
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("Đăng xuất thành công");
+
+            // Trả về JSON thay vì chuyển hướng
+            return new JsonResult(new { success = true, message = "Đăng xuất thành công" });
+        }
+
+        public IActionResult OnGet()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
